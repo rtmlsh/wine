0234be8a5a3bdac3063931e56ab1af2e94b7ef35
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-09-19 19:21:07 +0300

    Оформил код

--- main.py
+++ main.py
@@ -1,23 +1,47 @@
+import collections
 import datetime
-
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-template = env.get_template('template.html')
+def read_excel():
+    products = pandas.read_excel(
+        'wine.xlsx',
+        usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция'],
+        na_values=['N/A', 'NA'],
+        keep_default_na=False
+    )
+    return products
 
 
-rendered_page = template.render(
-    age_winery=datetime.date.today().year - 1921,
-)
+def get_goods_description(products):
+    store_goods = collections.defaultdict(list)
+    for product in products.to_dict(orient='records'):
+        title = product['Категория']
+        store_goods[title].append(product)
+    return store_goods
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+if __name__ == '__main__':
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+
+    products = read_excel()
+    store_goods = get_goods_description(products)
+
+    rendered_page = template.render(
+        age_winery=datetime.date.today().year - 1921,
+        store_goods=store_goods
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
