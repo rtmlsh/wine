01f7c6cb260ab476ec9f1bd7d78d91959e9d2c4b
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-10-20 22:14:02 +0300

    Добавил исключение в gitignore

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import collections
 import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
@@ -15,6 +16,9 @@ def get_goods_description(goods_specification):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Скрипт запускает сайт интернет-магазина')
+    parser.parse_args()
+
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
